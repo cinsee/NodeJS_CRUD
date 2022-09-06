5b81b06ad72bcc2552c9996d5e26e6e5c176b5de
Amorn <amorntepofficial@gmail.com>
2022-09-07 01:53:36 +0700

    update aollowedOrigin

--- configs/allowedOrigin.js
+++ configs/allowedOrigin.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
+    'https://nodejs-test-00-01.herokuapp.com/',
     'http://127.0.0.1:3000',
     'http://localhost:3500',
     'http://localhost:3000'

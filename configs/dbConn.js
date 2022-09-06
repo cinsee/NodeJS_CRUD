7d3cc46854156294ed7328202794de9b1e0b0522
cinsee <104057840+cinsee@users.noreply.github.com>
2022-09-07 05:14:45 +0700

    Update dbConn.js

--- configs/dbConn.js
+++ configs/dbConn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const dbUrl = 'mongodb+srv://mooncz:YVi4Zf7bHK7gBC35@cluster0.uht1nnj.mongodb.net/?retryWrites=true&w=majority'
+const dbUrl = process.env.MONGODB_URI
 
 const connectDB = async () => {
     try {
